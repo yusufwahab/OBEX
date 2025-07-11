3578fbb69370c5986b906b2719fd379baec8ecbd
yusufwahab <122736960+yusufwahab@users.noreply.github.com>
2025-07-11 20:05:03 +0100

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,86 +1,106 @@
-import { useState } from "react";
-import primusLogo from './logo-primus.png';
+import React, { useState, useEffect } from "react";
+import {useCameraStore} from './store/camera-store';
+import useLoadingStore from './store/loading-store';
+import LogoLoader from './LogoLoader';
+import CameraCard from "./CameraCard";
+import Header from "./Header";
 
-export default function Header({ addCameraStream }) {
-  const [menuOpen, setMenuOpen] = useState(false);
+export default function Dashboard() {
 
+  const [showMain, setShowMain] = useState(true)
+
+  const {showLoading, hideLoading} = useLoadingStore();
+  useEffect(() => {
+    showLoading();
+    setShowSection(false)
+    setShowMain(false)
+    const timer = setTimeout(() => {
+      hideLoading();
+      setShowSection(true)
+      setShowMain(true)
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+  
+
+  const [showSection, setShowSection] = useState(true)
+
+  const CameraStreams = useCameraStore((state) => state.CameraStreams)
+
+  const addToCameraStreams = useCameraStore((state) => state.addToCameraStreams)
+
+  const allcameras = [
+  { id: 1, cameraName: "Entrance Camera", date: "2025-07-07", time: "21:30", threatLevel: "Low" },
+  { id: 2, cameraName: "Backyard Cam", date: "2025-07-07", time: "21:30", threatLevel: "Medium" },
+  { id: 3, cameraName: "Office Cam", date: "2025-07-07", time: "21:30", threatLevel: "High" },
+  { id: 4, cameraName: "Lobby Cam", date: "2025-07-07", time: "21:30", threatLevel: "Low" },
+  { id: 5, cameraName: "Hallway Cam", date: "2025-07-07", time: "21:30", threatLevel: "Medium" },
+];
+const cameraElement = CameraStreams.map(cam => (
+  <CameraCard key={cam.id} {...cam} />
+))
+
+
+
+
+  function addCameraStream () {
+    if (CameraStreams.length < allcameras.length) {
+    showLoading()
+    setTimeout(() => {
+      addToCameraStreams(allcameras[CameraStreams.length]);
+      hideLoading();
+    }, 2000);
+  }
+  }
+
+  function handleClicks () {
+    addCameraStream()
+  }
+
+
+  
   return (
-    <nav className="bg-gray-800/80 backdrop-blur-md shadow-lg sticky top-0 z-50 border-b border-gray-700">
-      <div className="mx-4 sm:mx-6 lg:mx-10">
-        <div className="flex items-center justify-between h-16 xl:h-20">
-          {/* Logo */}
-          <a className="flex items-center">
-            <img
-              src={primusLogo}
-              alt="primus logo"
-              className="w-10 h-10 xl:w-10 xl:h-10 cursor-pointer"
-            />
-            <span className="text-2xl font-bold text-white cursor-pointer ml-2">
-              Primus
-              <span className="text-cyan-400 font-light font-black">Lite</span>
-            </span>
-          </a>
+    <>
+    <Header />
+    <LogoLoader />
+    {showMain && <main className="mt-10 bg-gray-800 w-[90vw] h-auto   m-auto rounded-lg shadow shadow-cyan-400/50 mb-10 pb-10 pt-5 xl:w-[95vw]">
+<article className="flex justify-between items-center m-5">
+    <figure className="flex text-3xl items-center gap-2 ml-12">
+    <svg className="h-8 w-8 md:h-8 md:w-8 lg:h-10 lg:w-10 text-cyan-400 "
+                    fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                    <path strokeLinecap="round"
+                        d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                </svg>
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold  text-gray-100">Live Feed</h1>
+      </figure>
+      {/* Disable "+" When All Cameras Are Added */}
+      {CameraStreams.length < allcameras.length && <button onClick={handleClicks} className="cursor-pointer text-sm  text-gray-100 px-3 py-2 rounded-lg animate-bounce outline-2 outline-cyan-400 mr-10 lg:mr-20" ><i className="fa fa-plus" aria-hidden="true"></i></button>}
+    </article>
 
-          {/* Hamburger (Mobile) */}
-          <div className="md:hidden">
-            <button
-              onClick={() => setMenuOpen(!menuOpen)}
-              className="text-white focus:outline-none"
-              aria-label="Toggle Menu"
-            >
-              <svg
-                className="w-6 h-6 text-cyan-400 cursor-pointer"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {menuOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                )}
-              </svg>
-            </button>
-          </div>
 
-          {/* Nav Links (Desktop) */}
-          <div className="hidden md:flex space-x-1 items-center">
-            {["Home", "Dashboard", "History", "Settings"].map((text) => (
-              <a
-                key={text}
-                className="text-white px-3 py-2 rounded-md text-sm font-medium hover:outline-1 hover:outline-cyan-800 cursor-pointer focus:bg-gray-700 active:bg-gray-700"
-              >
-                {text}
-              </a>
-            ))}
-          </div>
-        </div>
+    {/* Hide “Add Stream” Section When Cameras Exist */}
+    {CameraStreams.length === 0  && <section className="flex justify-center items-center">
+    <article className="grid place-content-center place-items-center bg-black/50 w-[80vw] h-[70vh] rounded-lg outline outline-cyan-900">
+    <svg className="h-20 w-20 lg:h-40 lg:w-40  xl:h-60 xl:w-60 text-cyan-400 " fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round"
+    d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+    </svg>
+      <h2 className="text-[20px] lg:text-[40px] font-bold text-gray-100 mb-6 mt-6"> Add Camera Stream</h2>
+      <p className="text-sm md:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-gray-100 text-center w-[60%] mb-6">This will allow you to connect a new camera feed to the dashboard in real time. 
+      Make sure the stream URL is accessible and correctly connected.</p>
+    </article>
+    </section> }
 
-        {/* Nav Links (Mobile dropdown) */}
-        {menuOpen && (
-          <div className="flex flex-col md:hidden mt-2 space-y-1 pb-4">
-            {["Home", "Dashboard", "History", "Settings"].map((text) => (
-              <a
-                key={text}
-                className="text-white px-3 py-2 rounded-md text-sm font-medium hover:outline-1 hover:outline-cyan-800 cursor-pointer focus:bg-gray-700 active:bg-gray-700"
-              >
-                {text}
-              </a>
-            ))}
-          </div>
-        )}
-      </div>
-    </nav>
+      
+
+      <section className=" grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3  gap-y-10  place-items-center 2xl:grid-cols-3 m-auto">
+
+      {/* Show Camera Streams, Declared by mapping through the cameraStreams array */}
+        {cameraElement}
+    
+      </section>
+
+    </main> }
+    </>
   );
 }
